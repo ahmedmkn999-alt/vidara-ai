2bc8415f8fa72fa9f991c6451fb3f5a8fe2b832b
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-25 21:46:34 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,32 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  // تعريف الموفرين (جوجل)
+  providers: [
+    GoogleProvider({
+      // سحب المفاتيح من متغيرات البيئة في Vercel
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // مفتاح السر لتشفير الجلسة
+  secret: process.env.NEXTAUTH_SECRET,
+  // تخصيص الجلسة
+  callbacks: {
+    async session({ session, token }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      return token;
+    }
+  },
+  // الصفحات المخصصة
+  pages: {
+    signIn: '/', // هيرجع للمستخدم لصفحة الباقات لو مش مسجل
+  }
+});
+
+// تصدير المعالج للطلبات من نوع GET و POST
+export { handler as GET, handler as POST };
 
